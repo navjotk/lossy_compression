7e6f2689c99851b2d839f829e7ab7523be635522
Navjot Kukreja <navjotk@gmail.com>
2018-10-03 10:37:00 +0100

    Add plots for wavefields

--- plot_numpy_hdf5.py
+++ plot_numpy_hdf5.py
@@ -15,11 +15,19 @@ with h5py.File(input_file) as ifile:
 velocity_model='overthrust_3D_initial_model.h5'
 
 model = from_hdf5(velocity_model, space_order=2, nbpml=20, datakey='m0', dtype=np.float32)
-
+shape = model.vp.shape
 vmax = np.max(data)
 
 slice_loc = 440
-plt.imshow(np.transpose(data[slice_loc]), vmin=-vmax, vmax=vmax, cmap="seismic")
-plt.imshow(np.transpose(model.vp[slice_loc]), alpha=0.5)
-plt.colorbar()
-plt.savefig(output_file)
+im = plt.imshow(np.transpose(data[slice_loc]), vmax=.01, vmin=-.01, cmap="seismic",
+           extent = [0, 20, 0.001*(shape[-1]-1)*25, 0])
+
+plt.xlabel("X (km)")
+plt.ylabel("Depth (km)")
+cb = plt.colorbar(shrink=.3, pad=.01, aspect=10)
+for l in cb.ax.yaxis.get_ticklabels():
+    l.set_fontsize(12)
+
+cb.set_label('Pressure')
+
+plt.savefig(output_file, bbox_inches='tight')
