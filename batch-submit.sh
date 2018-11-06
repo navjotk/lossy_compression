045cdb3e7965fac14d7ece10c35a11ff204712d4
Navjot Kukreja <navjotk@gmail.com>
2018-11-06 21:09:39 +0000

    Changes inplace

--- batch-submit.sh
+++ batch-submit.sh
@@ -11,7 +11,7 @@ source activate devito
 export PYTHONPATH=$PYTHONPATH:/home/nkukreja1/checkpointer
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/nkukreja1/compression/zfp-0.5.3/lib:/home/nkukreja1/intel/lib
 python /home/nkukreja1/devito/scripts/clear_devito_cache.py
-python /home/nkukreja1/compression/compression_ratios_over_time.py -so 32
+python /home/nkukreja1/compression/full_experiment.py --ncp $NCP
 #[ -z "$CHUNK_SIZE" ] && echo "Need to set CHUNK SIZE" && exit 1;
 #[ -z "$ALGO" ] && echo "Need to set ALGO" && exit 1;
 #[ -z "$SHUFFLE" ] && echo "Need to set SHUFFLE" && exit 1;
--- full_experiment.py
+++ full_experiment.py
@@ -64,17 +64,17 @@ def verify(space_order=4, kernel='OT4', nbpml=40, filename='', **kwargs):
     print("Verification took %d ms for forward and %d ms for reverse" % (tf.elapsed, tr.elapsed))
     
 
-def run(space_order=4, kernel='OT4', nbpml=40, filename='', **kwargs):
-    solver = acoustic_setup(shape=(10, 10), spacing=(10, 10), nbpml=10, tn=50,
-                            space_order=space_order, kernel=kernel, **kwargs)
-    #solver = overthrust_setup(filename=filename, tn=50, nbpml=nbpml, space_order=space_order, kernel=kernel, **kwargs)
+def run(space_order=4, ncp=None, kernel='OT4', nbpml=40, filename='', **kwargs):
+    #solver = acoustic_setup(shape=(10, 10), spacing=(10, 10), nbpml=10, tn=50,
+    #                        space_order=space_order, kernel=kernel, **kwargs)
+    solver = overthrust_setup(filename=filename, tn=1000, nbpml=nbpml, space_order=space_order, kernel=kernel, **kwargs)
     
     u = TimeFunction(name='u', grid=solver.model.grid, time_order=2, space_order=solver.space_order)
     rec = Receiver(name='rec', grid=solver.model.grid,
                               time_range=solver.receiver.time_range,
                               coordinates=solver.receiver.coordinates.data)
     cp = DevitoCheckpoint([u])
-    n_checkpoints = None
+    n_checkpoints = ncp
     m = solver.model.m
     dt = solver.dt
     v = TimeFunction(name='v', grid=solver.model.grid, time_order=2, space_order=solver.space_order)
@@ -101,6 +101,7 @@ if __name__ == "__main__":
     parser = ArgumentParser(description=description)
     parser.add_argument("-so", "--space_order", default=6,
                         type=int, help="Space order of the simulation")
+    parser.add_argument("--ncp", default=None, type=int)
     parser.add_argument("--nbpml", default=40,
                         type=int, help="Number of PML layers around the domain")
     parser.add_argument("-k", dest="kernel", default='OT2',
@@ -119,6 +120,6 @@ if __name__ == "__main__":
         space_order=args.space_order, kernel=args.kernel,
         dse=args.dse, dle=args.dle, filename='overthrust_3D_initial_model.h5')
     
-    run(nbpml=args.nbpml, 
+    run(nbpml=args.nbpml, ncp=args.ncp,
         space_order=args.space_order, kernel=args.kernel,
         dse=args.dse, dle=args.dle, filename='overthrust_3D_initial_model.h5')
